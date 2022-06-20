7f9d8896501cf7ea294e29627d83e091b50fbd90
Okan Sahin <okan.shn@hotmail.com>
2022-06-20 22:52:26 +0300

    singly linked list eklendi.

---
+++ singly_linked_lists.js
@@ -0,0 +1,128 @@
+// Piece of data - val
+// reference to next node - next
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0
+    }
+
+    push(val){
+       var newNode = new Node(val)
+        if(!this.head ){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++
+        return this;
+    }
+
+    pop(){
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+    shift()
+    {
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        
+        this.length++
+        return this;
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++
+        }
+    }
+
+    set(index, val){
+        var foundNode = this.get(index);
+        if(foundNode){
+            foundNode.val = val
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, val){
+        if(index < 0 || index >= this.length) return false;
+        if(index === this.length) return !!this.push(value);
+        if(index === 0) return !!this.unshift(val);
+        var newNode = new Node(val);
+        var prev = this.get(index - 1);
+        var temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        var prevNode = this.get(index - 1);
+        var removed = prevNode.next;
+        this.length--;
+        prevNode.next = removed.next;
+
+        return removed;
+        
+    }
+
+    // Sık soruluyor
+    reverse(){
+        
+    }
+}
+
+var list = new SinglyLinkList();
+
