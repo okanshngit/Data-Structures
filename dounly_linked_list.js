6fd5ec8ad887379eed7daee4865ef214349af738
Okan Sahin <okan.shn@hotmail.com>
2022-06-21 15:01:34 +0300

    list.push() eklendi.

--- dounly_linked_list.js
+++ dounly_linked_list.js
@@ -6,10 +6,26 @@ class Node {
     }
 }
 
+//Methods: push, 
 class DoublyLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
         this.lng = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val);
+        if(this.lng === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+            
+        }else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode
+        }
+        this.lng++;
+        return this;
     }
 }
