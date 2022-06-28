eba3de56afda9d6ea2cff74c5d200972e1f2bd89
Okan Sahin <okan.shn@hotmail.com>
2022-06-28 16:57:35 +0300

    breadth first search added.

--- binary_search_trees.js
+++ binary_search_trees.js
@@ -56,4 +56,23 @@ class BinarySearchTree {
     } 
 
     // 28.06.22
+    // Breadth First Search 
+    bfs(){
+        var data = [];
+        var queue = [];
+        var node = this.root;
+        queue.push(node);
+
+        while(queue.length){
+            node = queue.shift();
+            data.push(node);
+            if(node.left){
+                queue.push(node.left)
+            }
+            if(node.right){
+                queue.push(node.right)
+            }
+            return data;
+        }
+    }
 }
